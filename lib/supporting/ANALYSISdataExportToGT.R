8cbe167201ede369a6f655a4a9019ab0d85e050f
d.woodrich1@gmail.com <Daniel.Woodrich@nmfs.local>
2021-07-16 15:10:33 -0700

    add a GT to build soundchecker AL method

--- lib/supporting/ANALYSISdataExportToGT.R
+++ lib/supporting/ANALYSISdataExportToGT.R
@@ -1,8 +1,10 @@
 #load in FG, subset ANALYSIS data file to that FG, and export as format GT with i_neg_SC labels. 
 
+source("C:/Apps/INSTINCT/lib/supporting/instinct_fxns.R") 
+
 #define mooring
 
-Mooring<-"BS17_AU_PM02-b"
+Mooring<-"BS17_AU_PM02-a"
 
 #load in FG
 
@@ -10,7 +12,7 @@ FG<-read.csv(paste("C:/Apps/INSTINCT/Data/FileGroups/",Mooring,"_files_All.csv",
 
 #load in ANALYSIS tab
 
-ANALYSIStab<-read.csv("//nmfs/akc-nmml/CAEP/Acoustics/ANALYSIS/RWupcallYeses4Dan.csv")
+ANALYSIStab<-read.csv("//nmfs/akc-nmml/CAEP/Acoustics/ANALYSIS/HumpbackYeses_2.csv")
 
 #mandate column names to fit standard (column names changed on Cath end)
 colnames(ANALYSIStab)[1:7]<-c("Wavefile","StartSecInWav","EndSecInWav","MooringSite","MooringDeployID","StartFieldTimeUTC","EndFieldTimeUTC")
@@ -19,6 +21,11 @@ colnames(ANALYSIStab)[1:7]<-c("Wavefile","StartSecInWav","EndSecInWav","MooringS
 #subset ANALYSIS tab to mooring ID, 
 
 GT<-ANALYSIStab[which(ANALYSIStab$MooringDeployID==Mooring),]
+
+GTnew<-data.frame(GT$StartSecInWav,GT$EndSecInWav,0,800,getFileName(GT$Wavefile),getFileName(GT$Wavefile),"y","SC","HB")
+colnames(GTnew)<-c("StartTime","EndTime","LowFreq","HighFreq","StartFile",	"EndFile",	"label",	"Type",	"SignalCode")
+
+write.csv(GTnew,"C:/Apps/INSTINCT/Data/GroundTruth/HB/HB_BS17_AU_PM02-a_files_All.csv",row.names = FALSE)
 
 
 #format GT to standard
