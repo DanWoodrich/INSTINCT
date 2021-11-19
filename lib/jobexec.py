0949a9fc42b0e6ef1463bddeb923bb1baace7c25
d.woodrich1@gmail.com <Daniel.Woodrich@nmfs.local>
2021-11-18 17:48:47 -0800

    add a few more files to autoignore for user_push

--- lib/jobexec.py
+++ lib/jobexec.py
@@ -25,7 +25,7 @@ if (sys.argv[1]=='push_user'):
    
 
     #iteratively delete files and folders from contrib
-    files_to_ignore_contrib = ['__pycache__', 'README.txt','.git']
+    files_to_ignore_contrib = ['__pycache__', 'README.md','LICENSE','.git']
     
     for item in os.listdir(contribdir):
         if item not in files_to_ignore_contrib:  # If it isn't in the list for retaining
@@ -36,7 +36,7 @@ if (sys.argv[1]=='push_user'):
                 shutil.rmtree(path_item)  # Remove the dir
 
     #iteratively copy files and folders from user
-    files_to_ignore_user = ['__pycache__', 'README.txt','contrib']
+    files_to_ignore_user = ['__pycache__', 'README.md','LICENSE','contrib']
     
     for item in os.listdir(userdir):
         if item not in files_to_ignore_user:
